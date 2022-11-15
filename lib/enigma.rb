be467d167697fc69371379e9d15ed1ada8ac4a47
Elle Majors <hellolovely@ellemajors>
2022-11-15 12:45:21 -0700

    Add enigma class

---
+++ lib/enigma.rb
@@ -0,0 +1,43 @@
+class Enigma
+  attr_reader :message,
+              :key,
+              :date,
+              :ciphertext
+  def initialize(attributes)
+    @message = attributes[:message]
+    @key = attributes[:key]
+    @date = attributes[:date]
+    @ciphertext = attributes[:ciphertext]
+  end
+
+  def keys
+    { :A => @key.slice(0, 2),
+      :B => @key.slice(1, 2),
+      :C => @key.slice(2, 2),
+      :D => @key.slice(3, 2) }
+  end
+
+  def offsets
+    offset = [Math.sqrt(@date).to_i].slice(-1, 4)
+    { :A => offset[0],
+      :B => offset[1],
+      :C => offset[2],
+      :D => offset[3] }
+      # missing something but what
+  end
+end
+  # def encrypt(message, key, date)
+
+  # end
+
+  # def decrypt(ciphertext, key, date)
+    
+  # end
+
+  # def crack(ciphertext, date)
+
+  # end
+# end
+
+# character set ("a".."z").to_a << " "
+# keys 
